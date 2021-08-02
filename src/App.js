196ad2358fda274a53ee4bdddcba40da6ab3eb94
Damian Deza <damiandeza@gmail.com>
2021-08-01 23:40:44 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import NavBar from './components/navbar'
+import './App.sass';
+
+function App() {
+  return <div>
+    <NavBar></NavBar>
+  </div>
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./navbar.sass";
+
+// Uso un espacio vacio ya que necesito que sea así por el flex grow y para que no moleste con el hover cursor
+const categories = ['Catalogo', 'Cotizador', 'Contacto', 'Acerca de', "", 'Log In']
+
+class NavBar extends React.Component{
+    render(){
+        return <div className={"navBar"}>
+            <h1>CT@Home</h1>
+            {
+            categories.map(category => <p className={"navItem"}>{category}</p>)
+            }
+        </div>
+    }
+}
+
+export default NavBar;
