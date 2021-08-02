751fc47b0e6410ccef69f833f12cd60d0abc8d42
Damian Deza <damiandeza@gmail.com>
2021-08-02 01:35:33 -0300

    Agregado on click test

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,12 +4,14 @@ import "./navbar.sass";
 // Uso un espacio vacio ya que necesito que sea así por el flex grow y para que no moleste con el hover cursor
 const categories = ['Catalogo', 'Cotizador', 'Contacto', 'Acerca de', "", 'Log In']
 
+
+
 class NavBar extends React.Component{
     render(){
         return <div className={"navBar"}>
             <h1>CT@Home</h1>
             {
-            categories.map(category => <p className={"navItem"}>{category}</p>)
+            categories.map(category => <p onClick={()=>console.log(category)} className={"navItem"}>{category}</p>)
             }
         </div>
     }
