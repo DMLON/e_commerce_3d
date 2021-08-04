b616b69b87ec169d60b233e89b2832a5aef96de0
Damian Deza <damiandeza@gmail.com>
2021-08-03 22:37:03 -0300

    Cambiado widget de carrito

--- src/components/cartWidget.js
+++ src/components/cartWidget.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import "./cartWidget.sass";
 
 export default class CartWidget extends Component {
+    constructor(props){
+        super(props);
+        this.state = {cartElements: 1}
+    }
+    
     render() {
         return (
             <div className={"cart"}>
                 <span className={"iconify"} data-icon={"mdi:cart-outline"} data-inline={"false"}></span>
+                <span>{this.state.cartElements} elemento{this.state.cartElements == 1 ? "":"s"}</span>
+                <span class="caret"></span>
             </div>
         )
     }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,39 +1,54 @@
-import React from 'react';
+import React from "react";
 import "./navbar.sass";
-import CartWidget from './cartWidget';
-import SearchBar from './searchBar';
+import CartWidget from "./cartWidget";
+import SearchBar from "./searchBar";
 
+const categories = ["Catalogo", "Cotizador", "Contacto", "Acerca de", "Log In"];
 
-const categories = ['Catalogo', 'Cotizador', 'Contacto', 'Acerca de', 'Log In']
+function NavBarToggler(props) {
+    return (
+        <div>
+            <button
+                className={"navbar-toggler"}
+                type={"button"}
+                data-bs-toggle={"collapse"}
+                data-bs-target={`#${props.target}`}
+                aria-controls={props.target}
+                aria-expanded={"false"}
+                aria-label={"Toggle navigation"}
+            >
+                <span className={"navbar-toggler-icon"}></span>
+            </button>
+        </div>
+    );
+}
 
-
-
-class NavBar extends React.Component{
-
-    renderCategory(category){
-        return <li key={category} className={"nav-item"}>
-            <a className={"nav-link active"} aria-current={"page"}>{category}</a>
-        </li>
+class NavBar extends React.Component {
+    renderCategory(category) {
+        return (
+            <li key={category} className={"nav-item"}>
+                <a className={"nav-link active"} aria-current={"page"}>
+                    {category}
+                </a>
+            </li>
+        );
     }
-    render(){
-        return <nav className={"navbar navbar-expand-lg navbar-light bg-soft-yellow"}>
-            <div className={"container-fluid"}>
-                <a className={"navbar-brand"}>CT@Home</a>
-                <button className={"navbar-toggler"} type={"button"} data-bs-toggle={"collapse"} data-bs-target={"#navbarToggler"} aria-controls={"navbarToggler"} aria-expanded={"false"} aria-label={"Toggle navigation"}>
-                    <span className={"navbar-toggler-icon"}></span>
-                </button>
-                <div className={"collapse navbar-collapse"} id={"navbarToggler"}>
-                    <ul className={"navbar-nav me-auto mb-2 mb-lg-0"}>
-                        {
-                        categories.map(category => this.renderCategory(category))
-                        }
-                    </ul>
-                    <SearchBar/>
+    render() {
+        return (
+            <nav className={"navbar navbar-expand-lg navbar-light bg-soft-yellow"}>
+                <div className={"container-fluid"}>
+                    <a className={"navbar-brand"}>CT@Home</a>
+                    <NavBarToggler target={"navbarToggler"}/>
+                    <div className={"collapse navbar-collapse"} id={"navbarToggler"}>
+                        <ul className={"navbar-nav me-auto mb-2 mb-lg-0"}>
+                            {categories.map((category) => this.renderCategory(category))}
+                        </ul>
+                        <CartWidget />
+                    </div>
+                    {/* https://codepen.io/thalesmelo/pen/LRYwQo */}
                 </div>
-                <CartWidget/>
-            </div>
-            
-        </nav>
+            </nav>
+        );
     }
 }
 
