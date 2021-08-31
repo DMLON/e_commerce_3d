0999d5e910906d8d4ce31f4eb8497f299fc7af72
Damian Deza <damiandeza@gmail.com>
2021-08-30 23:18:26 -0300

    Agregado base para Firebase

---
+++ src/firebaseInit.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAzxu3IpLQc4wM90WUDTpbKdz-xQPt6SiY",
+    authDomain: "e-commerce-3d.firebaseapp.com",
+    projectId: "e-commerce-3d",
+    storageBucket: "e-commerce-3d.appspot.com",
+    messagingSenderId: "266024669886",
+    appId: "1:266024669886:web:ac2a2333daa8bf172804ef",
+    measurementId: "G-K67R3R08WZ"
+};
+
+const app = firebase.initializeApp(firebaseConfig)
+
+// Initialize Firebase
+export const getFirebase = () => app;
+export const getFirestore = () => firebase.firestore(app);
